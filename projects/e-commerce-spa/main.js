e86ba0319e13948f658d923e7fc15f34144eccaf
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-12-19 10:42:37 +0100

    update

--- projects/e-commerce-spa/main.js
+++ projects/e-commerce-spa/main.js
@@ -2,10 +2,42 @@
 
 let cart = [];
 
+const nav = document.querySelector("nav");
+
+const pages = document.querySelectorAll(".pages");
+pages.forEach((p) => {
+  const link = document.createElement("a");
+  
+  link.textContent = p.id;
+  link.href = "#" + p.id;
+
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    changePage(p.id)    
+  })
+
+  nav.append(link);
+  nav.append(" ");
+});
+
+
+function changePage(page) {
+  const pages = document.querySelectorAll(".pages");
+  pages.forEach((p) => p.classList.add("hidden"));
+
+  document.querySelector(`#${page}`).classList.remove("hidden");
+}
+
 function addToCart(product) {
   cart.push(product);
   console.log("Add to cart:", product);
   console.log("Cart:", cart);
+
+  updateCart()
+}
+
+function removeFromCart(product) {
+  updateCart()
 }
 
 document.querySelectorAll("button").forEach((button) => {
@@ -13,3 +45,17 @@ document.querySelectorAll("button").forEach((button) => {
     addToCart(event.target.parentElement.id)
   );
 });
+
+function updateCart() {
+  const cartHTML = document.querySelector("#cart-list");
+  cartHTML.innerHTML = "";
+
+  cart.forEach(p => {
+    const li = document.createElement("li");
+    li.textContent = p;
+
+    cartHTML.append(li);
+  })
+}
+
+changePage("product01");

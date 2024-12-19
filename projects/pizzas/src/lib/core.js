9b99402f483c517504933f42e2f89ffa94b7c4b1
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-12-19 12:52:15 +0100

    update

--- projects/pizzas/src/lib/core.js
+++ projects/pizzas/src/lib/core.js
@@ -1,10 +1,19 @@
 export function createElement(tag, ...children) {
   return {
     tag: tag,
-    children: children
-  }
+    children: children,
+  };
 }
 
-export function render(template, container) {
-  container.innerHTML = template;
+export function render(vNode, container) {
+  if (typeof vNode === "string") {
+    container.appendChild(document.createTextNode(vNode));
+    return;
+  }
+
+  const element = document.createElement(vNode.tag)
+
+  vNode.children.forEach(child => render(child, element))
+
+  container.appendChild(element);
 }
--- projects/pizzas/src/main.js
+++ projects/pizzas/src/main.js
@@ -7,13 +7,8 @@ let pizzas = [
   "Pizza 4 fromages",
   "Pizza chorizo",
   "Pizza montagnarde",
-  // "Pizza spéciale"
+  "Pizza spéciale",
 ];
-
-const titleTemplate = `<h1>Hello, ${title}</h1>`;
-const root = document.querySelector("#root");
-
-render(titleTemplate, root);
 
 const app = createElement(
   "app",
@@ -21,11 +16,12 @@ const app = createElement(
   createElement("h2", "Notre carte des pizzas :"),
   createElement(
     "ul",
-    createElement("li", "Pizza reine", createElement("button", "+")),
-    createElement("li", "Pizza 4 fromages", createElement("button", "+")),
-    createElement("li", "Pizza chorizo", createElement("button", "+")),
-    createElement("li", "Pizza montagnarde", createElement("button", "+"))
-  )
+    ...pizzas.map((p) => createElement("li", p, createElement("button", "+")))
+  ),
+  createElement("p", "Votre sélection :")
 );
 
-console.log(JSON.stringify(app));
+const root = document.querySelector("#root");
+render(app, root);
+
+console.log(app);
